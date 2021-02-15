8f23c63a3848aa68610ca2a4eb0b8cdf9dd3ff70
jjbmsda <jjbmsda@naver.com>
2021-02-16 00:58:03 +0900

    source upload

---
+++ Coding/BinaryGap.py
@@ -0,0 +1,4 @@
+Python 3.7.2rc1 (v3.7.2rc1:75a402a217, Dec 11 2018, 18:05:25) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
