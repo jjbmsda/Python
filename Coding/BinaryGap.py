79f0accf5468a320410abe3198312c366ac1fbf3
jjbmsda <jjbmsda@naver.com>
2021-02-16 00:59:14 +0900

    source upload

--- Coding/BinaryGap.py
+++ Coding/BinaryGap.py
@@ -1,4 +1,7 @@
-Python 3.7.2rc1 (v3.7.2rc1:75a402a217, Dec 11 2018, 18:05:25) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N):
+    # write your code in Python 3.6
+    pass
+
